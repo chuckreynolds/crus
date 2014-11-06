5c28b25504e4189634e0beca2836429170c60838
ChuckReynolds <chuck@vuurr.com>
2014-11-06 13:02:04 -0800

    update GA univ code

--- index.php
+++ index.php
@@ -64,7 +64,7 @@ $cr_desc = 'Chuck is a consultant for technical on-site SEO and conversion optim
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-1944161-3', 'chuckreynolds.us');
+  ga('create', 'UA-1944161-3', 'auto');
   ga('send', 'pageview');
 </script>
 </head>
