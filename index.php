4b2fee958b4f0fb6d0a25a396cb7e4286a5f0036
ChuckReynolds <chuck@vuurr.com>
2014-12-01 03:32:58 -0800

    add GTM, rm old tracking

--- index.php
+++ index.php
@@ -59,17 +59,17 @@ $cr_desc = 'Chuck is a consultant for technical on-site SEO and conversion optim
 	<meta name="twitter:creator" content="chuckreynolds">
 	<meta name="twitter:image:src" content="https://chuckreynolds.us/chuck_reynolds.jpg">
 	<meta name="twitter:domain" content="ChuckReynolds.us">
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-1944161-3', 'auto');
-  ga('require', 'displayfeatures');
-  ga('send', 'pageview');
-</script>
 </head>
 <body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-K5LWNG"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K5LWNG');</script>
+<!-- End Google Tag Manager -->
 <div class="container" itemscope itemtype="http://schema.org/Person">
 	<div class="arrow_down_high"></div>
 	<div class="row">
@@ -202,18 +202,6 @@ $cr_desc = 'Chuck is a consultant for technical on-site SEO and conversion optim
 <? /*
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 */ ?>
-<script type="text/javascript">
-var clicky_site_ids = clicky_site_ids || [];
-clicky_site_ids.push(100794784);
-(function() {
-  var s = document.createElement('script');
-  s.type = 'text/javascript';
-  s.async = true;
-  s.src = '//static.getclicky.com/js';
-  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
-})();
-</script>
-<noscript><p><img alt="Clicky" width="1" height="1" src="//in.getclicky.com/100794784ns.gif" /></p></noscript>
 </body>
 </html>
 <? /* list exists here now: https://gist.github.com/chuckreynolds/4227ee3ad9536bc1a02e */ ?>
