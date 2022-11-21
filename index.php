04da31f528ba02bccbb9bf0084b8dfb746e71744
Chuck Reynolds <chuck@vuurr.com>
2022-11-21 14:10:42 -0700

    add impact verify code

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ $ChuckDesc  = "Chuck is a consultant for technical on-site SEO, web performance,
 	<meta name="description" content="<?php echo $ChuckDesc; ?>">
 	<meta name="referrer" content="origin-when-crossorigin">
 	<meta name="microid" content="mailto+http:sha1:518acdbdf5c826329e9f75e98bae9bd42b242f5b">
+	<meta name='ir-site-verification-token' value='-2082428433'>
 	<link rel="me" href="https://micro.blog/chuck">
 	<link rel="canonical" href="https://chuckreynolds.us">
 	<link rel="alternate" type="application/rss+xml" title="UpChuck - Atypical posts from an Average Chuck" href="https://upchuck.us/feed">
